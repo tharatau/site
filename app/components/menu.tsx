607f538554e539f8a1222b728e8a12b211302457
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-02-08 14:03:36 -0500

    chore: disable link to blog

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -4,15 +4,20 @@ export default function Menu() {
             <a href="/" className="flex-1 mx-12 hover:text-zinc-400">
                 AC
             </a>
+            
+            <a href="https://resume.ayushmanchhabra.com/" rel="noreferrer" target="_blank" className="flex-2 mx-2 text-2sm hover:text-zinc-400">
+                WEB RESUME
+            </a>
             <a href="https://www.linkedin.com/in/ayushmanchhabra/" rel="noreferrer" target="_blank" className="flex-2 mx-2 text-2sm hover:text-zinc-400">
                 LINKEDIN
             </a>
             <a href="https://github.com/ayushmanchhabra/" rel="noreferrer" target="_blank" className="flex-2 mx-2 text-2sm hover:text-zinc-400">
                 GITHUB
             </a>
+            {/* FIXME
             <a href="/blog" className="flex-2 mx-2 text-2sm hover:text-zinc-400">
                 BLOG
-            </a>
+            </a> */}
         </nav>
     );
 }
