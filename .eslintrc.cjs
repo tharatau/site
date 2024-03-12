2146d20a03219892579a8616f4248bf3187b335a
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-03-12 19:16:54 +0530

    chore: migrate to vite (#7)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,18 @@
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-
-  // Base config
-  extends: ["eslint:recommended"],
-};
+}
---
+++ App.tsx
@@ -0,0 +1,19 @@
+export default function App() {
+
+  return (
+    <div className='h-screen w-screen p-4'>
+      <div className='w-full flex items-end'>
+        <span className='mx-0'>CV</span>
+        <span className='mx-4'>Portfolio</span>
+        <span className='mx-4'>Services</span>
+        <span className='mx-0'>Blog</span>
+      </div>
+
+      <div className='h-full flex items-center justify-center flex-col'>
+        <span className='text-4xl m-5'>Ayushman Chhabra</span>
+        <span className='text-lg'>Programmer, Musician, Fitness Enthusiast</span>
+      </div>
+
+    </div>
+  )
+}
