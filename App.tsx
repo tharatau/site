d34c37afcdb8d3c99bbc3903388b20af4812b88d
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-03-15 12:16:19 +0530

    chore: hide menu

--- App.tsx
+++ App.tsx
@@ -3,15 +3,14 @@ export default function App() {
   return (
     <div className='h-max p-4'>
       <div className='w-full flex items-end'>
-        <span className='mx-0'>CV</span>
-        <span className='mx-2'>Portfolio</span>
-        <span className='mx-2'>Services</span>
-        <span className='mx-2'>Blog</span>
+        {false && (<><span className='mx-0 hover:underline transition ease-in-out'>CV</span>
+        <span className='mx-2 hover:underline transition ease-in-out'>Portfolio</span>
+        <span className='mx-2 hover:underline transition ease-in-out'>Services</span>
+        <span className='mx-2 hover:underline transition ease-in-out'>Blog</span></>)}
       </div>
 
       <div className='h-full flex flex-col'>
         <br />
-
         <span className='text-2xl'>Ayushman Chhabra</span>
         <br />
         <ul>
