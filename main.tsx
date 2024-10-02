151a017833389af05dd36a564cabfbd536e47b96
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-10-02 15:45:35 +0530

    feat: add support for articles
    * post assumptions

--- main.tsx
+++ main.tsx
@@ -1,25 +1,44 @@
 import React from "vsx";
 
+import posts from './posts/index';
+
 import "./main.css";
 
-function App (): Element {
-    
+function App(): Element {
+
     return (
-        <div style={{
-            height: '100%',
-            width: '100%',
-            margin: '10px'
-        }}>
-            <h3>Ayushman Chhabra</h3>
-            <p>
-                Economics student turned software developer turned security analyst.
-            </p>
-            <p>
-                Interested in music, literature and fitness.
-            </p>
-            <p>
-                Occassionally touches grass.
-            </p>
+        <div>
+            <article>
+                <h3>Ayushman Chhabra</h3>
+                <p>
+                    Economics student turned software developer turned security analyst.
+                </p>
+                <p>
+                    Interested in music, literature and fitness.
+                </p>
+                <p>
+                    Occassionally touches grass.
+                </p>
+            </article>
+            <br />{"-x-"}<br /><br />
+            {posts.map((post) => (
+                <article>
+                    <h3>{post.name}</h3>
+                    <span><b>Published:</b> {post.date.published} <b>Updated:</b> {post.date.updated}</span>
+                    <p>
+                        {post.content.split("\n").map((para) => (
+                            <p>{para}</p>
+                        ))}
+                    </p>
+                    <br />
+                    <h4>References: </h4>
+                    <p>
+                        {post.reference.map((ref: string) => (
+                            <a href={ref}>{ref}</a>
+                        ))}
+                    </p>
+                </article>
+            ))}
         </div>
     );
 }
---
+++ posts/index.ts
@@ -0,0 +1,6 @@
+import post_20240916 from './2024.09.16.json';
+
+const posts = [post_20240916];
+
+export default posts;
+
